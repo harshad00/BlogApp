4e87fa9e23b97e372b52af7e37c4a20ced7a1a47
Harshad 200 <bholo0090@gmail.com>
2024-06-02 02:35:41 +0530

    update postcc.c.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
   },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
